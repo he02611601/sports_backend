64515f91470eb5b4a2d527c4e492a48218b478a6
PaulChen <he02611601@gmail.com>
2021-08-08 11:57:49 +0800

    [Change] adjust ping router response data

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
-			"data": "This is data",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
